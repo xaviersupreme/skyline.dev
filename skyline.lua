8c9005638353b43fdb2ed3c6a9817350dc50a81a
xavier <162805460+xaviersupreme@users.noreply.github.com>
2025-07-30 23:22:01 -0700

    Update skyline.lua

--- skyline.lua
+++ skyline.lua
@@ -20,7 +20,7 @@ local Decimals = 4
 local Clock = os.clock()
 local ValueText = "Value Is Now :"
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/drillygzzly/Roblox-UI-Libs/main/1%20Tokyo%20Lib%20(FIXED)/Tokyo%20Lib%20Source.lua"))({
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaviersupreme/skyline.dev/refs/heads/main/lib/UI%20Library.lua"))({
     cheatname = "skyline.dev",
     gamename = "",
 })
